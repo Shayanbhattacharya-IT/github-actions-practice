fe78454a4129480bbedb1bd757a1beca915ceb29
Shayan_Bhattacharya_09 <87561616+Shayanbhattacharya-IT@users.noreply.github.com>
2026-06-20 23:27:29 +0530

    Clean up app.py by removing unused code
    Removed incomplete function definition and comments.

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-def run_
-    do
+# this is my 3rd github actions
 
 @app.route('/health')
 def health():
